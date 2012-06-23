8d3b2b72828d28746545e09bb33ffa3f832cc4f7
Adam Kirkton <akirkton@truefitsolutions.com>
2012-06-23 00:48:48 -0400

    Example using Object.create for object creation
    Shows an example of using Object.create to create an object that inherits
    from another object.

--- Car.js
+++ Car.js
@@ -1,2 +1,36 @@
-function Car(year, make, model) {
-}
+var Car = (function () {
+  function drainOldOil(car) {
+    console.log(car.quartsOfOil + ' quarts of oil drained.');
+  }
+
+  function addNewOil(car) {
+    console.log(car.quartsOfOil + ' quarts of oil added.');
+  }
+
+  return {
+    year: null,
+    make: null,
+    model: null,
+    quartsOfOil:  5,
+    engineStarted: false,
+
+    changeOil: function () {
+      if (this.engineStarted) {
+        console.error("You can't change the oil with the car running!");
+      } else {
+        drainOldOil(this);
+        addNewOil(this);
+      }
+    },
+
+    startEngine: function () {
+      this.engineStarted = true;
+      console.log('Started the engine of the ' + this.year + ' ' + this.make + ' ' + this.model + ' car.');
+    },
+
+    stopEngine: function () {
+      this.engineStarted = false;
+      console.log('Stopped the engine of the ' + this.year + ' ' + this.make + ' ' + this.model + ' car.');
+    }
+  };
+}());
